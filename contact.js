030287a83fff51b5fe3f2fdec8fd93bf78b86a0e
Preetimaurya0611 <preetimaurya236@gmail.com>
2026-01-18 10:09:54 +0530

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,20 @@
+document.getElementById("contactForm").addEventListener("submit", async function (e) {
+  e.preventDefault();
+
+  const formData = new FormData(this);
+
+  try {
+    const res = await fetch("/contact", {
+      method: "POST",
+      body: formData
+    });
+
+    if (!res.ok) throw new Error("Server error");
+
+    document.getElementById("msg").innerText = "Form submitted successfully!";
+    this.reset();
+
+  } catch (err) {
+    document.getElementById("msg").innerText = "Server error! Backend not reachable";
+  }
+});
